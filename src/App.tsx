49d24fb26011b9135fce98b70aa5e09432fd7af4
gevored <gersonrevoredo@hotmail.com>
2021-08-07 01:01:40 -0300

    finished firebase  authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import {BrowserRouter, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
 import {Home} from './pages/Home'
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <h1>
-      <Home/>
-    </h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path = "/" exact component = {Home}/>
+        <Route path = "/rooms/news" component = {NewRoom}/>
+      </AuthContextProvider>
+     
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,27 @@
+
+import {useHistory} from 'react-router-dom'
 import  illustrationImg from '../assets/images/illustration.svg'
 import  logoImg from '../assets/images/logo.svg'
 import  googleIconImg from '../assets/images/google-icon.svg'
 
 import '../style/auth.scss'
 import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
 
 export function Home(){
+
+    const history = useHistory()
+    const {user , signWithGoogle} = useAuth()
+    
+    async function handleCreateRoom(){ 
+
+        if(!user){
+            await signWithGoogle()
+        }
+        history.push('/rooms/news')   
+    }
+
     return(
         <div id = "page-auth">
             <aside>
@@ -14,7 +30,7 @@ export function Home(){
                 <p>tire as dúvidas da su audiência em tempo real</p>
             </aside>
             <main>
-                <div className="main-content">
+                <div onClick = {handleCreateRoom} className="main-content">
                     <img src={logoImg} alt="letmeask" />
                     <button className="create-room" >
                         <img src={googleIconImg} alt="" />
